7813aa33ea761c72068f504449ad7e20608fb590
Florian <79457058+florian-alb@users.noreply.github.com>
2025-03-13 00:43:14 +0100

    ✨ refactor application structure by removing AppController and AppService, adding UserController, and implementing DatabaseModule for database connection management

---
+++ server/src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+import { DATABASE_CONNECTION } from './database-connection';
+
+import { Pool } from 'pg';
+
+// helps to provide a single instance of our database into the application
+@Module({
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        });
+        return drizzle(pool, {
+          schema: {},
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: [DATABASE_CONNECTION],
+})
+export class DatabaseModule {}

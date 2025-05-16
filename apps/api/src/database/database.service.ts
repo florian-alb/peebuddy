7f17a0ec8719daadfe30d39f0e848364792b2cad
Florian <79457058+florian-alb@users.noreply.github.com>
2025-05-16 15:54:25 +0200

    fix(api): #9 update database service and fix backend port

--- apps/api/src/database/database.service.ts
+++ apps/api/src/database/database.service.ts
@@ -9,7 +9,8 @@ export class DatabaseService
   async onModuleInit() {
     await this.$connect();
   }
-  async onModuleDestroy() {
-    await this.$disconnect;
+
+  onModuleDestroy() {
+    this.$disconnect;
   }
 }

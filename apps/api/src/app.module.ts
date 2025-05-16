f65f5d6bb03d8361f4e66cf8c3d65d13f985ac14
Florian <79457058+florian-alb@users.noreply.github.com>
2025-05-16 15:19:54 +0200

    feat(api): #9 upgrade NestJS and Prisma dependencies, add database module and initial schema

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,10 +4,13 @@ import { LinksModule } from './links/links.module';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { UserService } from './user/user.service';
+import { DatabaseModule } from './database/database.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [LinksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LinksModule, DatabaseModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}

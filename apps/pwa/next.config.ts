278bc0ed8390cf88588e94a23a05998ac83ac06f
Florian <79457058+florian-alb@users.noreply.github.com>
2025-07-03 08:56:08 +0200

    fix(build) : fixed build on vercel

--- apps/pwa/next.config.ts
+++ apps/pwa/next.config.ts
@@ -9,6 +9,15 @@ const withPWA = require("next-pwa")({
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@workspace/ui"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const {
+        PrismaPlugin,
+      } = require("@prisma/nextjs-monorepo-workaround-plugin");
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,17 +1,25 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
   async rewrites() {
     return [
       {
-        source: '/api/auth/:path*',
-        destination: '/api/auth/:path*',
+        source: "/api/auth/:path*",
+        destination: "/api/auth/:path*",
       },
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*",
       },
-    ]
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
   },
 };
 
